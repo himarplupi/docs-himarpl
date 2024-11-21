bca8a07e969134bd3810153207d414bf91b5a90f
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-11-21 20:14:57 +0700

    feat: add sidebar autogenerate folders

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,20 +8,69 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Documentation HIMARPL",
+      defaultLocale: "root",
+      locales: {
+        // English docs in `src/content/docs/en/`
+        root: {
+          label: "English",
+          lang: "en",
+        },
+        // Indonesia docs in `src/content/docs/id/`
+        id: {
+          label: "Indonesia",
+          lang: "id",
+        },
+      },
       social: {
         github: "https://github.com/himarplupi",
       },
       sidebar: [
         {
-          label: "Guides",
+          label: "Getting Started",
           autogenerate: {
-            directory: "guides",
+            directory: "getting-started",
           },
         },
         {
-          label: "Reference",
+          label: "Overview Projects",
           autogenerate: {
-            directory: "reference",
+            directory: "overview-projects",
+          },
+        },
+        {
+          label: "A. www-himarpl",
+          autogenerate: {
+            directory: "www-himarpl",
+          },
+        },
+        {
+          label: "B. blog-himarpl",
+          autogenerate: {
+            directory: "blog-himarpl",
+          },
+        },
+        {
+          label: "C. pmb-himarpl",
+          autogenerate: {
+            directory: "pmb-himarpl",
+          },
+        },
+        {
+          label: "D. mabim-himarpl",
+          autogenerate: {
+            directory: "mabim-himarpl",
+          },
+        },
+        {
+          label: "E. docs-himarpl",
+          autogenerate: {
+            directory: "docs-himarpl",
+          },
+        },
+        {
+          label: "Other Projects",
+          autogenerate: {
+            directory: "other-projects",
           },
         },
       ],
